09d140fe398a01f475730cde697f0c81514aca63
TDxDimitri <talleu.dimitri@outlook.fr>
2023-03-06 14:39:02 +0100

    maj feature describe

--- php/class/Person.php
+++ php/class/Person.php
@@ -4,12 +4,19 @@ class Person {
     // Attributs
     private $_lastName;
     private $_firstName;
+    private $_color;
+
+    const PREFERED_COLOR_RED = "Rouge";
+    const PREFERED_COLOR_BLUE = "Bleu";
+    const PREFERED_COLOR_GREEN = "Vert";
+    const PREFERED_COLOR_YELLOW = "Jaune";
 
     // Constructor
-    public function __construct($lastName="Nom",$firstName="Prénom")
+    public function __construct($lastName = "Nom", $firstName = "Prénom", $color="Color")
     {
         $this->setLastName($lastName);
         $this->setFirstName($firstName);
+        $this->setColor($color);
         
     }
     // Getters
@@ -21,6 +28,10 @@ class Person {
         return $this->_firstName;
 
     }
+    public function getColor() {
+        return $this->_color;
+
+    }
     // Setters
     public function setLastName($lastName) {
         if (!preg_match('/\d/', $lastName)){
@@ -28,11 +39,36 @@ class Person {
     }
 
     }
+
     public function setFirstName($firstName) {
         if (!preg_match('/\d/', $firstName)){
             $this->_firstName = $firstName;
         }
     
     }
+    /**
+     * Sélectionner une couleur parmis les couleurs proposées
+     * Tapez Person::
+     */
+    public function setColor($color) {
+            if(in_array($color, [self::PREFERED_COLOR_RED, self::PREFERED_COLOR_BLUE, self::PREFERED_COLOR_GREEN, self::PREFERED_COLOR_YELLOW])) {
+        $this->_color = $color;
+        }
+    }
     // Methods
+    
+    
+    /**
+     * describe
+     *Description d'une instance de l'objet Person
+     * @return string
+     */
+    public function describe() :string {
+
+    return 'Le nom est : '.$this->_lastName. ' , Le prénom est : '. $this->_firstName.',  La couleur préférée est le: '. $this->_color;
+
+    
+}
+
+
 }
--- php/index.php
+++ php/index.php
@@ -4,7 +4,7 @@ require './class/person.php';
 
 $logan = new Person;
 $dimitri = new Person;
-$theo = new Person("FEVER","Theo");
+$theo = new Person("FEVER","Theo","Violet");
 
 $logan->setFirstName('Logan');
 $logan->setLastName('HENON');
@@ -15,3 +15,12 @@ $dimitri->setLastName('TALLEU');
 echo "<br>". $logan->getFirstName()  . " " .  $logan->getLastName();
 echo "<br>". $dimitri->getFirstName()  . " " .  $dimitri->getLastName();
 echo "<br>". $theo->getFirstName()  . " " .  $theo->getLastName();
+
+
+    $loganDescribe = $theo->describe();
+
+    echo "<br>" . $loganDescribe;
+
+$logan->setColor(Person::PREFERED_COLOR_YELLOW);
+
+echo "<br>". $logan->getColor();
